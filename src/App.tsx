4a38d31dda159da8afe16b8e624aa057ed605511
Rafael de Luca <deluca1712@gmail.com>
2023-04-03 15:54:53 -0300

    Customizando o bootstrap com node-sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
 
-    <h2>Produtos Melhoramentos</h2>
+    <h2 className="bg-secondary txt-primary">Produtos Melhoramentos</h2>
     
   );
 }
