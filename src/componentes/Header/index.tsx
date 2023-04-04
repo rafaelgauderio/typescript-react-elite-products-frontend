fbc4ec01eacb55425bbd0fd6de3d4fff0179f5d4
Rafael de Luca <deluca1712@gmail.com>
2023-04-04 13:51:11 -0300

    icone menu

--- src/componentes/Header/index.tsx
+++ src/componentes/Header/index.tsx
@@ -4,15 +4,29 @@ import 'bootstrap/js/src/collapse';
 
 
 function Header() {
-  return (
+    return (
 
-    <nav className='navbar navbar-expand-md cabecalho'>
-        <header className='container-fluid' >            
+        <nav className='navbar navbar-expand-md navbar-light cabecalho'>
+            <header className='container-fluid' >
                 <a href="link logo" className='texto-logo'>
                     <h3>Logo Melhoramentos</h3>
                 </a>
-                <div className='collapse navbar-collapse'>                    
-                    <ul className='menu-principal navbar-nav offset-md-3'>
+                
+                    <button
+                        className="navbar-toggler"
+                        type="button"                        
+                        data-bs-toggle="collapse"
+                        data-bs-target="#menu-hamburguer-navbar"  
+                        aria-controls="menu-hamburguer-navbar"
+                        aria-expanded="false"
+                        arial-label="Toggle navigation"                    
+                    >
+                        <p>menu</p>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div id="menu-hamburguer-navbar" className='collapse navbar-collapse'>
+                    <ul className='menu-principal navbar-nav offset-md-2'>
                         <li>
                             <a href="link home" className='ativo'>
                                 HOME
@@ -37,13 +51,13 @@ function Header() {
                             <a href="link admim">
                                 PAINEL ADMIN
                             </a>
-                        </li>                        
+                        </li>
                     </ul>
-                </div>                     
-        </header>
-    </nav>
-  
-  );
+                </div>
+            </header>
+        </nav>
+
+    );
 }
 
 export default Header;
