57a1f118d810243c32b4bd75d61fc2678976147c
Rafael de Luca <deluca1712@gmail.com>
2023-04-06 16:26:20 -0300

    Adicionando alguns ProdutoCard mocados via bootstrap

--- src/paginas/Produtos/index.tsx
+++ src/paginas/Produtos/index.tsx
@@ -4,15 +4,48 @@ import Header from '../../componentes/Header';
 import Rodape from '../../componentes/Rodape/intex';
 import './styles.css';
 
-function Produtos () {
+function Produtos() {
     return (
-        <>        
-        <Header />
+        <>
+            <Header />
             <div className="container w-100 h-100 my-5 produtos-container">
                 <h2>Listagem de Produtos</h2>
-                <CardProduto />
+                <div className="row">
+                    <div className="col-xl-3 col-lg-4 col-sm-6">
+                        <CardProduto />
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-sm-6">
+                        <CardProduto />
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-sm-6">
+                        <CardProduto />
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-sm-6">
+                        <CardProduto />
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-sm-6">
+                        <CardProduto />
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-sm-6">
+                        <CardProduto />
+                    </div> <div className="col-xl-3 col-lg-4 col-sm-6">
+                        <CardProduto />
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-sm-6">
+                        <CardProduto />
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-sm6">
+                        <CardProduto />
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-sm-6">
+                        <CardProduto />
+                    </div>
+                    <div className="col-xl-3 col-lg-4 col-sm-6">
+                        <CardProduto />
+                    </div>
+                </div>
             </div>
-        <Rodape />
+            <Rodape />
         </>
     )
 
