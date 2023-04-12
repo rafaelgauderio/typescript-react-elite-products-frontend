a1b9a52ee7d0024d481967aee46e2079c858d54d
Rafael de Luca <deluca1712@gmail.com>
2023-04-12 12:49:36 -0300

    Estilização da page Detalhes do Produto

--- src/paginas/ProdutoDetalhado/index.tsx
+++ src/paginas/ProdutoDetalhado/index.tsx
@@ -9,27 +9,27 @@ function ProdutoDetalhado() {
                     <button className="btn btn-primary botao-voltar">Voltar ao catálogo</button>
                 </div>
                 <div className="row">
-                    <div className="col-xl-7">
+                    <div className="col-xl-5">
                         <div className="imagem-container">
                             <img src={require("../../assets/imagens/7127.png")} alt="imagem do produto"></img>
                         </div>
                         <div className="descricao-container">
-                            <h3>Papel Higiênico Rolo 7127</h3>
+                            <h2>Papel Higiênico Rolo 7127</h2>
                         </div>
                     </div>
-                    <div className="col-xl-5">
+                    <div className="col-xl-7">
                         <div className="descricao-embalagem-categoria-container">
-                            <div className="categorias-cotainer">
-                                <h3>Categorias do Produto</h3> 
+                            <div className="categorias-container">
+                                <h3>Categorias do produto</h3> 
                                 <p>Folha Dupla</p>
                                 <p>Papel higiênico</p> 
                             </div>                            
-                            <div className="embalagens-cotainer">  
-                                <h3>Embalagens disponíveis</h3> 
+                            <div className="embalagens-container">  
+                                <h3>Descrição da embalagem</h3> 
                                 <p>2.400 metros</p>
                                 <p>Fardo com 8 rolos</p>                             
                             </div>
-                            <h3>Descrição completa  do produto</h3>
+                            <h3>Descrição completa</h3>
                             <p>Papel higiênico em rolo 100% fibras virgens, Folha Dupla, Super Macio, Folhas Brancas</p>
                         </div>
                     </div>
