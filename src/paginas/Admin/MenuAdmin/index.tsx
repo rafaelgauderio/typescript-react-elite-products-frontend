76a8d75109ccfb431a0b3f4685e7a0c50c454f92
Rafael de Luca <deluca1712@gmail.com>
2023-04-13 16:52:16 -0300

    Layout do painel do administrador

---
+++ src/paginas/Admin/MenuAdmin/index.tsx
@@ -0,0 +1,41 @@
+import './styles.css';
+
+function Menu() {
+
+    return (
+        <nav>
+            <ul className="link-primary">
+
+                <li>
+                    <a href="link-sms-email" className="admin-menu-link">
+                        <p>Email /sms</p>
+                    </a>
+                </li>
+
+
+                <li>
+                    <a href="link-produtos" className="admin-menu-link">
+                        <p>Produtos</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="link-embalagens" className="admin-menu-link">
+                        <p>Embalagens</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="link-categorias" className="admin-menu-link">
+                        <p>Categorias</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="link-usuarios" className="admin-menu-link">
+                        <p>Usuários</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Menu;
