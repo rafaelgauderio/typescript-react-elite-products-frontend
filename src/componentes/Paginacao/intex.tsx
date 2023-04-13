9c76032b50ef9cdefaec90007597c0722ac99d1e
Rafael de Luca <deluca1712@gmail.com>
2023-04-13 15:40:24 -0300

    estilização dos icones de paginação

--- src/componentes/Paginacao/intex.tsx
+++ src/componentes/Paginacao/intex.tsx
@@ -1,19 +1,20 @@
-import { ReactComponent as DirecionalProximo } from "../../assets/imagens/direciona-proximo.svg";
+import { ReactComponent as DirecionalProximo } from "../../assets/imagens/direcional-proximo.svg";
+import { ReactComponent as DirecionalAnterior } from "../../assets/imagens/direcional-anterior.svg";
 import './styles.css';
 
 function Paginacao() {
 
     return (
         <div className="paginacao-container">
-            <DirecionalProximo/>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-            <div>...</div>
-            <div>10</div>
-            <DirecionalProximo/>
+            <DirecionalAnterior className="direcional-anterior icone-ativado"/>
+            <div className="paginacao-icone">1</div>
+            <div className="paginacao-icone">2</div>
+            <div className="paginacao-icone">3</div>
+            <div className="paginacao-icone">4</div>
+            <div className="paginacao-icone">5</div>
+            <div className="paginacao-icone">...</div>
+            <div className="paginacao-icone ativado">10</div>
+            <DirecionalProximo className="direcional-proximo icone-desativado"/>
         </div>
 
     )
