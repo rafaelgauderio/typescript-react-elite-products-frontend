41b6dc3c18348a47bfe96cc6f17d5526d00d1da8
Rafael de Luca <deluca1712@gmail.com>
2023-04-14 13:40:26 -0300

    Conteudo da pagina de contatos

--- src/paginas/Contatos/index.tsx
+++ src/paginas/Contatos/index.tsx
@@ -1,18 +1,41 @@
 import CardContato from '../../componentes/CardContato';
 import './styles.css';
 
-function Contatos () {
+function Contatos() {
 
     return (
-        <div className="container my-4">
-            <div className="contato-card">
-            <CardContato></CardContato>
-            <CardContato></CardContato>
-            <CardContato></CardContato>
-            <CardContato></CardContato>
-            <CardContato></CardContato>
+        <div className="container my-4 my-lg-5 contato-container">
+            <div className="row titulo-container">
+                <h2>Entre em contato</h2>
+            </div>
+            <div className="row">
+                <div className="col-lg-4 col-sm-12">
+                    <CardContato></CardContato>
+                </div>
+                <div className="col-lg-4 col-sm-12">
+                    <CardContato></CardContato>
+                </div>
+                <div className="col-lg-4 col-sm-12">
+                    <CardContato></CardContato>
+                </div>
+                <div className="col-lg-4 col-sm-12">
+                    <CardContato></CardContato>
+                </div>
+                <div className="col-lg-4 col-sm-12">
+                    <CardContato></CardContato>
+                </div>
+                <div className="col-lg-4 col-sm-12">
+                    <CardContato></CardContato>
+                </div>
+                <div className="col-lg-4 col-md-12">
+                    <CardContato></CardContato>
+                </div>
+                <div className="col-lg-4 col-sm-12">
+                    <CardContato></CardContato>
+                </div>
             </div>
         </div>
+
     );
 }
 
