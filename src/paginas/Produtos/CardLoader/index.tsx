4578d676001e003f507c49e0ffacbc38407fc8b0
Rafael de Luca <deluca1712@gmail.com>
2023-04-18 13:32:27 -0300

    Loader da pagina de detalhes do Produto

--- src/paginas/Produtos/CardLoader/index.tsx
+++ src/paginas/Produtos/CardLoader/index.tsx
@@ -1,7 +1,7 @@
 import ContentLoader from 'react-content-loader';
 import './styles.css';
 
-const ImagemGrid = () => (
+const ProdutoImageLoader = () => (
     <div className="card-loader-container">
         <ContentLoader
             speed={1.3}
@@ -17,11 +17,11 @@ const ImagemGrid = () => (
 )
 
 
-ImagemGrid.metadata = {
+ProdutoImageLoader.metadata = {
     name: 'Hassan Tijani.A',
     github: 'surepeps',
     description: 'Image Grid with Pagination',
     filename: 'ImageGrid',
 }
 
-export default ImagemGrid;
+export default ProdutoImageLoader;
