18584e3c290c3cbc5f14d590bb5ddce9d2aa1ed5
Rafael de Luca <deluca1712@gmail.com>
2023-04-18 15:24:42 -0300

    Tela inicial de autenticacao

---
+++ src/paginas/Admin/MenuAdmin/Autenticar/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function Autenticacao() {
+
+    return (
+        <div className="container autenticacao-container">
+            <h2> Tela temporária de autenticação</h2>
+        </div>
+    );
+
+};
+
+export default Autenticacao;
