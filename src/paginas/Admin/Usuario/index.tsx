a8989f19716c43e4d8255e7954b1633f07f7e0ab
Rafael de Luca <deluca1712@gmail.com>
2023-04-25 10:46:23 -0300

    Refatorando pagina Usuario

--- src/paginas/Admin/Usuario/index.tsx
+++ src/paginas/Admin/Usuario/index.tsx
@@ -8,6 +8,7 @@ import './styles.css';
 
 const Usuarios = () => {
     const [pagina, setPagina] = useState<PaginaSpring<Usuario>>();
+
     useEffect(() => {
         const parametros: AxiosRequestConfig = {
             url: '/usuarios',
@@ -22,17 +23,16 @@ const Usuarios = () => {
         });
     }, []);
     return (
-        <div className="usuario-container">
-            {pagina?.content.map((item) => (
-                <>
-                    <div className="usuario-card">
-                        <p key={item.id}>{item.nome} {item.sobrenome}</p>
-                        <p key={item.id}>{item.email}</p>
-                        {item?.regras.map(regra => (
-                            <p className="usuario-regra">{regra.permissao}</p>
-                        ))}
-                    </div>
-                </>
+        <div className="usuario-container" >
+            {pagina?.content.map((usuario) => (
+                <div className="usuario-card" key={usuario.id}>
+                    <p key={usuario.id}> {usuario.nome} {usuario.sobrenome} <br />
+                        {usuario.email}</p>
+                    {usuario?.regras.map(regra => (
+                        <p>{regra.permissao}</p>
+                    ))}
+                </div>
+
             ))}
         </div>
     );
