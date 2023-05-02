6be51a57261ee9581b3b3cad885cb01cc8cf0ee4
Rafael de Luca <deluca1712@gmail.com>
2023-05-02 13:58:39 -0300

    Componente Card de Cadastro de Produto

---
+++ src/componentes/CardCadastroProduto/index.tsx
@@ -0,0 +1,25 @@
+import './styles.css';
+import { Produto } from '../../tipos/Produto';
+
+// Props são argumentos dos componentes Reacts.
+// componentes reactes são funções javaScript
+
+type Props = {
+  produto : Produto;  
+}
+
+function CardProduto ({produto} : Props)  {
+    
+  return (
+    <div className="produto-card">
+      <div className="produto-container-superior">
+        <img src={produto.imgUrl} alt="Imagem do Produto" />
+      </div>
+      <div className="produto-container-inferior">
+        <h4>{produto.descricao}</h4>        
+      </div>
+    </div>
+  );
+};
+
+export default CardProduto;
--- src/paginas/Admin/Produtos/ListagemProdutos/index.tsx
+++ src/paginas/Admin/Produtos/ListagemProdutos/index.tsx
@@ -1,8 +1,60 @@
+import CardCadastroProduto from "../../../../componentes/CardCadastroProduto";
+import './styles.css';
 
-function ListagemProdutos () {
+function ListagemProdutos() {
+
+    const produtoMocado = {
+
+        id: 1,
+        descricao: "Papel Higiênico Rolo 7127",
+        descricaoCompleta: "Papel higiênico em rolo 100% fibras virgens, Folha Dupla, Super Macio, Folhas Brancas",
+        preco: null,
+        largura: 10.0,
+        metragem: 300,
+        peso: null,
+        fragrancia: null,
+        imgUrl: "https://melhoramentoshigieners.com.br/imagens/7127.jpg",
+        dataCadastro: "2023-03-01T16:00:00.123450Z",
+        embalagens: [
+            {
+                id: 3,
+                descricao: "2.400 metros"
+            },
+            {
+                id: 1,
+                descricao: "Fardo com 8 rolos"
+            }
+        ],
+        categorias: [
+            {
+                id: 2,
+                descricao: "Papel Higiênico"
+            },
+            {
+                id: 6,
+                descricao: "Excellence"
+            },
+            {
+                id: 5,
+                descricao: "Rolo"
+            },
+            {
+                id: 10,
+                descricao: "Folha Dupla"
+            }
+        ]
+    }
 
     return (
-        <h1>Listagem de Produtos</h1>
+        <div className="container">
+            <button className="btn btn-primary botao-inserir">Inserir Novo</button>
+            <div className="barra-pesquisa">Caixa de Busca</div>
+
+            <CardCadastroProduto produto={produtoMocado}></CardCadastroProduto>
+            <CardCadastroProduto produto={produtoMocado}></CardCadastroProduto>
+            <CardCadastroProduto produto={produtoMocado}></CardCadastroProduto>
+            <CardCadastroProduto produto={produtoMocado}></CardCadastroProduto>
+        </div>
     )
 }
 
