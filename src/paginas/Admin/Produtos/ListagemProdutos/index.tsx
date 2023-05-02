6b23bf8567264c661ca80b953f119ef2adeac890
Rafael de Luca <deluca1712@gmail.com>
2023-05-02 13:31:17 -0300

    Rotas de listagem de cadastro de produtos

---
+++ src/paginas/Admin/Produtos/ListagemProdutos/index.tsx
@@ -0,0 +1,9 @@
+
+function ListagemProdutos () {
+
+    return (
+        <h1>Listagem de Produtos</h1>
+    )
+}
+
+export default ListagemProdutos;
---
+++ src/paginas/Admin/Produtos/index.tsx
@@ -0,0 +1,20 @@
+import { Route, Switch } from "react-router-dom";
+import CadastroProdutos from "./CadastroProdutos";
+import ListagemProdutos from "./ListagemProdutos";
+
+function ProdutosCadastro () {
+
+    return (
+        <Switch>
+            <Route path="/admin/produtos" exact>
+                <ListagemProdutos></ListagemProdutos>
+            </Route>
+            <Route path="/admin/produtos/:produtoId">
+                <CadastroProdutos></CadastroProdutos>
+            </Route>
+        </Switch>
+    )
+
+}
+
+export default ProdutosCadastro;
