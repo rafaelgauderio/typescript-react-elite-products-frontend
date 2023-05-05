9614b12d5a56f86fdb6d467bf41256abccc3d5d6
Rafael de Luca <deluca1712@gmail.com>
2023-05-05 14:07:40 -0300

    Validando demais campos do formulario de Cadastro Produto

--- src/paginas/Admin/Produtos/CadastroProduto/index.tsx
+++ src/paginas/Admin/Produtos/CadastroProduto/index.tsx
@@ -49,47 +49,115 @@ function CadastroProdutos() {
                                     type='text'
                                     className={`form-control input-padrao 
                                     ${errors.descricao ? 'is-invalid' : ''} `}
-                                    placeholder='Nome do Produto'
+                                    placeholder='Nome do produto'
                                     name='descricao' />
                                 <div className="invalid-feedback d-block">
                                     {errors.descricao?.message}
                                 </div>
                             </label>
                             <label>Peso:
-                                <input type="text" className="form-control input-padrao" />
+                                <input {
+                                    ...register('peso')}
+                                    type='text'
+                                    className={`form-control input-padrao`}
+                                    placeholder='Peso em Kg'
+                                    name='peso' />
                             </label>
                             <label>Largura:
-                                <input type="text" className="form-control input-padrao" />
+                                <input {
+                                    ...register('largura')}
+                                    type='text'
+                                    className={`form-control input-padrao`}
+                                    placeholder='Largura do folha/rolo em centímetros'
+                                    name='largura' />
                             </label>
                             <label>Metragem:
-                                <input type="text" className="form-control input-padrao" />
+                                <input {
+                                    ...register('metragem')}
+                                    type='text'
+                                    className={`form-control input-padrao`}
+                                    placeholder='Metragem total da embalagem em metros'
+                                    name='metragem' />
                             </label>
                             <label>Fragrância:
-                                <input type="text" className="form-control input-padrao" />
+                                <input {
+                                    ...register('fragrancia')}
+                                    type='text'
+                                    className={`form-control input-padrao`}
+                                    placeholder='Fragrância do produto'
+                                    name='fragrancia' />
                             </label>
                             <label>Imagem:
-                                <input type="text" className="form-control input-padrao" />
+                                <input {
+                                    ...register('imgUrl', {
+                                        required: 'Campo obrigatório',
+                                        minLength: {
+                                            value: 5,
+                                            message: 'Mínimo de 5 caracteres',
+                                        },
+                                        maxLength: {
+                                            value: 100,
+                                            message: 'Máximo de 100 caracteres',
+                                        },
+                                    })}
+                                    type='text'
+                                    className={`form-control input-padrao 
+                                    ${errors.descricao ? 'is-invalid' : ''} `}
+                                    placeholder='Link da imagem do produto'
+                                    name='imgUrl' />
+                                <div className="invalid-feedback d-block">
+                                    {errors.imgUrl?.message}
+                                </div>
                             </label>
                         </div>
                         <div className="col-lg-6">
                             <label>Categorias:
-                                <input type="text" className="form-control input-padrao" />
+                                <input {
+                                    ...register('categorias')}
+                                    type='text'
+                                    className={`form-control input-padrao`}
+                                    placeholder='Categorias do produto'
+                                    name='categorias' />
                             </label>
                             <label>Embalagens:
-                                <input type="text" className="form-control input-padrao" />
+                                <input {
+                                    ...register('embalagens')}
+                                    type='text'
+                                    className={`form-control input-padrao`}
+                                    placeholder='Embalagens disponíveis'
+                                    name='embalagens' />
                             </label>
                             <label>Descrição Detalhada:
-                                <textarea className="form-control input-padrao h-auto" rows={12}></textarea>
+                                <textarea rows={12}
+                                    {...register('descricaoCompleta', {
+                                        required: 'Campo obrigatório ',
+                                        minLength: {
+                                            value: 5,
+                                            message: 'Mínimo de 5 caracteres',
+                                        },
+                                        maxLength: {
+                                            value: 100,
+                                            message: 'Máximo de 100 caracteres',
+                                        },
+                                    })}
+                                    className={`form-control input-padrao h-auto
+                                ${errors.descricaoCompleta ? 'is-invalid' : ''} `}
+                                    placeholder='Descrição detalhada do produto'
+                                    name='descricaoCompleta' >
+                                </textarea>
                             </label>
+                            <div className="invalid-feedback d-block">
+                                {errors.descricao?.message}
+                            </div>
                         </div>
                     </div>
                     <div className="cadatro-produto-form-botoes-container">
                         <button className="btn btn-outline-danger botao-cancelar">CANCELAR</button>
                         <button className="btn btn-outline-primary botao-salvar">SALVAR</button>
                     </div>
-                </form>
-            </div>
-        </div>
+                </form >
+            </div >
+        </div >
     )
 }
 
