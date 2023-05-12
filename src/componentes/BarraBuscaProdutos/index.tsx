79acc0feefb8d7d311375d3df5db410eb221518d
Rafael de Luca <deluca1712@gmail.com>
2023-05-12 17:58:22 -0300

    Buscando categorias e embalagens do banckend para a barra de busca

--- src/componentes/BarraBuscaProdutos/index.tsx
+++ src/componentes/BarraBuscaProdutos/index.tsx
@@ -1,26 +1,98 @@
 import './styles.css';
 import { ReactComponent as IconeBusca } from '../../assets/imagens/icone-busca.svg';
+import { Controller, useForm } from 'react-hook-form';
+import { Embalagem } from '../../tipos/Embalgem';
+import { Categoria } from '../../tipos/Categoria';
+import Select from 'react-select';
+import { useEffect, useState } from 'react';
+import { requisicaoPadraoBackend } from '../../util/requisicao';
 
 function BarraBuscaProdutos() {
 
+    type DadosBarraBusca = {
+        descricao: string;
+        embalagem: Embalagem;
+        categoria: Categoria;
+    }
+
+    const {
+        register,
+        handleSubmit,
+        control,
+    } = useForm<DadosBarraBusca>();
+
+    const [selectEmbalagens, setSelectEmbalagens] = useState<Embalagem[]>([]);
+
+    const [selectCategorias, setSelectCategorias] = useState<Categoria[]>([]);
+
+    // a barra de busca é um formulario que faz um select no database com os filtros desejados
+    function buscarProdutoAoEnviar(dadosFormularioBusca: DadosBarraBusca) {
+        console.log("Enviado dados formulario!", dadosFormularioBusca);
+
+    }
+
+    useEffect(() => {
+        requisicaoPadraoBackend({ url: '/embalagens' })
+            .then((resposta) => {
+                setSelectEmbalagens(resposta.data.content);
+            });
+    }, []);
+
+    useEffect(() => {
+        requisicaoPadraoBackend({ url: '/categorias' })
+            .then((resposta) => {
+                setSelectCategorias(resposta.data.content);
+            });
+    }, []);
+
+
+
     return (
         <div className="barra-pesquisa-produto">
-            <form action="formulario-barra-pesquisa">
+            <form onSubmit={handleSubmit(buscarProdutoAoEnviar)} className="formulario-barra-pesquisa">
                 <div className="descricao-produto-barra-pesquisa">
                     <input
+                        {...register('descricao')}
+                        name='descricao'
                         type="text"
                         className="form-control"
                         placeholder='Busque aqui seu produto...'>
                     </input>
-                    <IconeBusca></IconeBusca>
+                    <button>
+                        <IconeBusca></IconeBusca>
+                    </button>
                 </div>
                 <div className="embalagem-categoria-limpar-container">
-                    <select>
-                        <option>Embalagem</option>
-                    </select>
-                    <select>
-                        <option>Categoria</option>
-                    </select>
+                    <Controller
+                        name="embalagem"
+                        control={control}
+                        render={({ field }) => (
+                            <Select
+                                {...field}
+                                options={selectEmbalagens}
+                                classNamePrefix={'cadastro-produto-select'}
+                                placeholder='Embalagem'
+                                isClearable={true}
+                                getOptionLabel={(embalagem: Embalagem) => embalagem.descricao}
+                                getOptionValue={(embalagem: Embalagem) => String(embalagem.id)}
+                            />
+                        )}
+                    />
+                    <Controller
+                        name="categoria"
+                        control={control}
+                        render={({ field }) => (
+                            <Select
+                                {...field}
+                                options={selectCategorias}
+                                classNamePrefix={'cadastro-produto-select'}
+                                placeholder='Categoria'
+                                isClearable={true}
+                                getOptionLabel={(categoria: Categoria) => categoria.descricao}
+                                getOptionValue={(categoria: Categoria) => String(categoria.id)}
+                            />
+                        )}
+                    />
                     <button className="btn btn-warning botao-limpar">Limpar</button>
                 </div>
             </form>
