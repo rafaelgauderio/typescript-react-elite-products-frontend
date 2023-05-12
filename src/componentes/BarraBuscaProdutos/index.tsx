6bb18a71944b4598194a169e4c6d25e407cd009c
Rafael de Luca <deluca1712@gmail.com>
2023-05-12 15:37:55 -0300

    Estrutura do componente de busca de produto

--- src/componentes/BarraBuscaProdutos/index.tsx
+++ src/componentes/BarraBuscaProdutos/index.tsx
@@ -1,10 +1,29 @@
 import './styles.css';
+import { ReactComponent as IconeBusca } from '../../assets/imagens/icone-busca.svg';
 
 function BarraBuscaProdutos() {
 
     return (
         <div className="barra-pesquisa-produto">
-            Caixa de Busca
+            <form action="formulario-barra-pesquisa">
+                <div className="descricao-produto-barra-pesquisa">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder='Busque aqui seu produto...'>
+                    </input>
+                    <IconeBusca></IconeBusca>
+                </div>
+                <div className="embalagem-categoria-limpar-container">
+                    <select>
+                        <option>Embalagem</option>
+                    </select>
+                    <select>
+                        <option>Categoria</option>
+                    </select>
+                    <button className="btn btn-warning botao-limpar">Limpar</button>
+                </div>
+            </form>
         </div>
     );
 
