3a00d86ed23dda902e030fd1ba7f439175cb06b2
Rafael de Luca <deluca1712@gmail.com>
2023-08-16 14:08:45 -0300

    Listando categorias dinamicamente do backend

--- src/paginas/Admin/Categorias/ListagemCategorias/index.tsx
+++ src/paginas/Admin/Categorias/ListagemCategorias/index.tsx
@@ -1,16 +1,55 @@
+import { AxiosRequestConfig } from "axios";
 import CardCadastroCategoria from "../../../../componentes/CardCadastroCategoria";
+import { requisicaoPadraoBackend } from "../../../../util/requisicao";
+import { PaginaSpring } from "../../../../tipos/biblioteca/spring";
+import { useCallback, useEffect, useState } from "react";
+import { Categoria } from "../../../../tipos/Categoria";
+
 import './styles.css';
 
 export default function ListagemCategorias() {
+
+    const [pagina, setPagina] = useState<PaginaSpring<Categoria>>();
+
+    const getCagegorias = useCallback(requisicaoCategorias, []);
+
+    useEffect(() => {
+        getCagegorias();
+    }, [getCagegorias]);
+
+    function requisicaoCategorias() {
+        const configuracao: AxiosRequestConfig = {
+            method: 'GET',
+            url: '/categorias',
+            params: {
+                size: 50,
+            }
+        };
+        requisicaoPadraoBackend(configuracao).then((resposta) => {
+            setPagina(resposta.data);
+        });
+    };
+
+
     return (<>
         <div className="cadastro-categoria-container">
-            <CardCadastroCategoria categoria={{
-                id: 0,
-                descricao: "Categoria Teste"
-            }}></CardCadastroCategoria>
-        </div>
+            <div className="row">
+                {pagina?.content.map((categoria) => (
+                    <div key={categoria.id}>
+                        <CardCadastroCategoria
+                            categoria={categoria}
+                        />
+                    </div>
+                ))
+
+                }
+            </div>
+        </div >
+
     </>
     );
 }
+
+
 
 
