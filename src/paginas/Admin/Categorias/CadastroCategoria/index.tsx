a110974de98a7e0803dafec4c2c88271d6cfa6ab
Rafael de Luca <deluca1712@gmail.com>
2023-08-17 17:33:30 -0300

    salvar um categoria nova

--- src/paginas/Admin/Categorias/CadastroCategoria/index.tsx
+++ src/paginas/Admin/Categorias/CadastroCategoria/index.tsx
@@ -7,6 +7,9 @@ import { toast } from "react-toastify";
 import historico from "../../../../util/historico";
 
 import './styles.css';
+import { requisicaoPadraoBackend } from "../../../../util/requisicao";
+import { AxiosRequestConfig } from "axios";
+import { useEffect } from "react";
 
 export type ParamentrosUrl = {
     categoriaId: string;
@@ -16,13 +19,63 @@ const CadastroCategorias = () => {
 
     const { categoriaId } = useParams<ParamentrosUrl>();
 
+
+    let editandoCategoria: boolean = categoriaId !== 'inserir' ? true : false;
+
     const rotaListagemCategorias: string = '/admin/categorias';
 
-    const { register, handleSubmit, formState: { errors } } = useForm<Categoria>();
+
+
+    const { register, handleSubmit, setValue, control, formState: { errors } } = useForm<Categoria>();
+
+    useEffect(() => {
+        if (editandoCategoria) {
+            requisicaoPadraoBackend({
+                url: `/categorias/${categoriaId}`,
+            })
+                .then((response) => {
+                    let categoria = response.data as Categoria;
+
+                    setValue('descricao', (categoria.descricao).toLowerCase());
+                })
+        }
+    }, [editandoCategoria, setValue, categoriaId]);
+
+
 
     function salvarCategoria(dadosFormularioCategoria: Categoria) {
 
-    }
+        const configInsertCategoria: AxiosRequestConfig = {
+            method: 'POST',
+            url: '/categorias',
+            data: dadosFormularioCategoria,
+            withCredentials: true,
+        };
+
+        const configUpdateCategoria: AxiosRequestConfig = {
+            method: 'PUT',
+            url: `/categorias/${categoriaId}`,
+            data: dadosFormularioCategoria,
+            withCredentials: true,
+        };
+
+        let config: AxiosRequestConfig = editandoCategoria === true ? configUpdateCategoria : configInsertCategoria;
+
+        requisicaoPadraoBackend(config)
+            .then((response) => {
+                toast.success("Categoria Cadastrada/Editada com sucesso", {
+                    hideProgressBar: false,
+                    theme: "colored",
+                    draggable: true
+                });
+                historico.push(rotaListagemCategorias);
+            }).catch(() => {
+                toast.error("Erro ao tentar cadastrar Categoria");
+            });
+    };
+
+
+
 
     function botaoCancelar() {
         toast.info('Cancelada a inserção/edição de CATEGORIA', {
