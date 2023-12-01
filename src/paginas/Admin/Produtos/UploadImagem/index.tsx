349014517c6117470e11a292296667e9a34599bd
Rafael de Luca <deluca1712@gmail.com>
2023-12-01 17:13:28 -0300

    Requisicao POST para upload de imagem

--- src/paginas/Admin/Produtos/UploadImagem/index.tsx
+++ src/paginas/Admin/Produtos/UploadImagem/index.tsx
@@ -1,16 +1,51 @@
 import React from 'react';
 import './styles.css';
 import { ReactComponent as ImagemPlaceholder } from '../../../../assets/imagens/image-placeholder.svg';
+import { requisicaoPadraoBackend } from '../../../../util/requisicao';
 
 function UploadImagem() {
+
+    function enviarImagem(listaImagensSelecionadas: File) {
+        const dados = new FormData();
+        dados.append('arquivo', listaImagensSelecionadas);
+
+        requisicaoPadraoBackend({
+            url: '/produtos/imagem',
+            method: 'POST',
+            data: dados,
+            withCredentials: true
+        })
+            .then(() => {
+                console.log("Imagem enviada com sucesso");
+            })
+            .catch(() => {
+                console.log("Erro ao tentar enviar imagem");
+            })
+    }
+
+
+    function handleOnChangeImage(evento: React.ChangeEvent<HTMLInputElement>) {
+        const listaImagensSelecionadas = evento.target.files?.[0];
+
+        //só subir imagem caso o usuário selecionou alguma imagem válida
+        if (listaImagensSelecionadas !== undefined) {
+            //console.log(listaImagensSelecionadas);
+            enviarImagem(listaImagensSelecionadas);
+
+        }
+
+    }
+
     return (
         <div className="row">
             <div className="col-6">
                 <div className="botao-upload-container">
                     <input
                         type="file"
-                        hidden
                         id="envioImagem"
+                        accept="image/jpg, image/jpeg, image/png"
+                        hidden
+                        onChange={handleOnChangeImage}
                     >
                     </input>
                     <label htmlFor="envioImagem">Upload Imagem</label>
